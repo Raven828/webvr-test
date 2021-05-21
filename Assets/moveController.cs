5f1bfd0f34da078ffac92c31a5c0c28ac2a845c6
Raven828 <ravenstream83@gmail.com>
2021-05-21 22:48:53 +0900

    First Commit
    webVのサンプルシーンに移動と視点移動機能を追加

---
+++ Assets/moveController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveController : MonoBehaviour
+{
+    [SerializeField]
+    float mainSPEED;
+    [SerializeField]
+    float x_sensi;
+    [SerializeField]
+    float y_sensi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        if (Input.GetMouseButton(0))
+        {
+            camera();
+        }
+            
+    }
+
+    void move()
+    {
+        Transform trans = transform;
+        transform.position = trans.position;
+        trans.position += trans.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * mainSPEED;
+        trans.position += trans.TransformDirection(Vector3.right) * Input.GetAxis("Horizontal") * mainSPEED;
+    }
+
+    void camera()
+    {
+        float x_Rotation = Input.GetAxis("Mouse X");
+        float y_Rotation = Input.GetAxis("Mouse Y");
+        x_Rotation = x_Rotation * x_sensi;
+        y_Rotation = y_Rotation * y_sensi;
+        this.transform.Rotate(0, x_Rotation, 0);
+        this.transform.Rotate(-y_Rotation, 0, 0);
+    }
+}
